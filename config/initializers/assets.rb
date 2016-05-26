fd62a2977aa77322cf9ce1ef51eefab133ef9932
Zuza <zuzanna.sawala@gmail.com>
2016-05-26 21:19:01 +0200

    real time validations

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,3 +11,4 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( search.js )
 
 Rails.application.config.assets.precompile += %w( icon_bar.coffee )
+Rails.application.config.assets.precompile += %w( rails.validations.js )
