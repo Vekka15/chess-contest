5a436b7c27f4f8939332a76ae1201c0df9808c8a
Zuza <zuzanna.sawala@gmail.com>
2016-05-28 14:40:44 +0200

    modified user model

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'tournaments#index'
   resources :tournaments, only: [:index, :new, :create, :show]
   resources :participations, only: [:create, :destroy]
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'users/registrations' }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
